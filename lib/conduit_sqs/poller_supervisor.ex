38ef510390254dd9f738b8712568dc62e17229ce
Allen Madsen <Allen Madsen>
2017-10-10 18:59:35 -0400

    Add child specs for pollers

--- lib/conduit_sqs/poller_supervisor.ex
+++ lib/conduit_sqs/poller_supervisor.ex
@@ -5,11 +5,21 @@ defmodule ConduitSQS.PollerSupervisor do
     Supervisor.start_link(__MODULE__, [subscribers, opts], name: __MODULE__)
   end
 
+  # %{
+  #   conduitsqs_test: {ConduitSQSTest.Subscriber, [from: ["conduitsqs-test"]]}
+  # },
   def init([subscribers, opts]) do
     import Supervisor.Spec
 
-    children = [
-    ]
+    children =
+      subscribers
+      |> Enum.flat_map(fn {_name, {_subscriber, opts}} ->
+        Enum.map(opts[:from], &{&1, opts})
+      end)
+      |> Enum.with_index()
+      |> Enum.map(fn {{queue, opts}, index} ->
+        worker(ConduitSQS.Poller, [queue, opts], id: {ConduitSQS.Poller, index})
+      end)
 
     supervise(children, strategy: :one_for_one)
   end
---
+++ test/conduit_sqs/poller_supervisor_test.exs
@@ -0,0 +1,32 @@
+defmodule ConduitSQS.PollerSupervisorTest do
+  use ExUnit.Case, async: true
+  alias ConduitSQS.PollerSupervisor
+
+  describe "init/1" do
+    @args [
+      %{
+        conduitsqs_test: {ConduitSQSTest.Subscriber, [from: ["conduitsqs-test"]]},
+        conduitsqs_test2: {ConduitSQSTest.Subscriber, [from: ["conduitsqs-test2", "conduitsqs-test3"]]}
+      },
+      []
+    ]
+    test "returns child specs for each queue" do
+      assert {:ok, {adapter_opts, child_specs}} = PollerSupervisor.init(@args)
+
+      # {strategy, max_restarts, max_seconds}
+      assert adapter_opts == {:one_for_one, 3, 5}
+
+      assert child_specs == [
+        {{ConduitSQS.Poller, 0}, {ConduitSQS.Poller, :start_link,
+          ["conduitsqs-test", [from: ["conduitsqs-test"]]]},
+          :permanent, 5000, :worker, [ConduitSQS.Poller]},
+        {{ConduitSQS.Poller, 1}, {ConduitSQS.Poller, :start_link,
+          ["conduitsqs-test2", [from: ["conduitsqs-test2", "conduitsqs-test3"]]]},
+          :permanent, 5000, :worker, [ConduitSQS.Poller]},
+        {{ConduitSQS.Poller, 2}, {ConduitSQS.Poller, :start_link,
+          ["conduitsqs-test3", [from: ["conduitsqs-test2", "conduitsqs-test3"]]]},
+          :permanent, 5000, :worker, [ConduitSQS.Poller]}
+      ]
+    end
+  end
+end
